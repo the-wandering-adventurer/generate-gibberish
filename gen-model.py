3c8cc47c59d97e175113610d3d834161f1886e4f
the-wandering-adventurer <150296506+the-wandering-adventurer@users.noreply.github.com>
2025-02-15 17:35:01 -0500

    initial commit of code

---
+++ gen-model.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+from itertools import accumulate
+import base64
+import gzip
+import json
+import re
+
+with open("/usr/share/dict/words") as f:
+    words = f.read().splitlines()
+
+words = [w for w in words if re.match(r"[a-z]+$", w)]
+
+one_char_words = [w for w in words if len(w) == 1]
+two_char_words = [w for w in words if len(w) == 2]
+start_chars = []
+model = defaultdict(lambda: defaultdict(int))
+
+for word in [w for w in words if len(w) > 2]:
+    prev = word[0:2]
+    start_chars.append(prev)
+    for c in word[2:]:
+        model[prev][c] += 1
+        prev = prev[1] + c
+
+for prev in model.keys():
+    model[prev] = dict(
+        accumulate(model[prev].items(), lambda a, b: (b[0], a[1] + b[1]))
+    )
+
+json = json.dumps(
+    dict(
+        one_char_words=one_char_words,
+        two_char_words=two_char_words,
+        start_chars=start_chars,
+        states=model,
+    )
+)
+model_encoded = base64.b64encode(gzip.compress(json.encode()))
+
+print(model_encoded.decode())
