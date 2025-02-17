7d6dd1bb691cead22fe50798d59d3f2059f46b6f
the-wandering-adventurer <150296506+the-wandering-adventurer@users.noreply.github.com>
2025-02-17 13:56:40 -0500

    add some comments to code

--- gen-gibberish.py
+++ gen-gibberish.py
@@ -4,32 +4,48 @@ import json
 import random
 import sys
 
+# Load the gzip'd JSON Markov chain
 with gzip.open(sys.argv[1], "rt") as f:
     model = json.load(f)
 
+# Extract the states and starting characters into global variables
+# (Just for easier reading down the road).
 states = model["states"]
 start_chars = model["start_chars"]
 
 
 def gen_word():
+    # Grab two random characters from the starter set
     word = random.choices(
         list(start_chars.keys()), cum_weights=list(start_chars.values())
     )
+    # random.choices always returns a list, even if you only want one element
     word = word[0]
+
+    # Now use the chain to generate a word
     while True:
+        # Grab the last two characters of the word to use as our context
         key = word[-2:]
+
+        # If the key is in our chain, then grab the next character at random
         if key in states:
             next_char = random.choices(
                 list(states[key].keys()), cum_weights=list(states[key].values())
             )
             next_char = next_char[0]
         else:
+            # The key isn't in the chain - we've reached the end
             break
         if next_char == ".":
+            # The "." is a special marker for end-of-word
             break
+
+        # We've gotten this far, add the character to the word and keep going
         word += next_char
     return word
 
+
+# Generate some random words
 num_words = random.randint(1, 4) + random.randint(1, 4) + 1
 sentence = [gen_word() for _ in range(0, num_words)]
 print(" ".join(sentence))
--- gen-model.py
+++ gen-model.py
@@ -16,23 +16,52 @@ with open(sys.argv[1]) as f:
 
 words = [w for w in words if re.match(r"^\w+$", w) and w.islower() and len(w) > 2]
 
+# We are going to keep track of
+#
+# - start_chars: The first two characters of every word.
+#   These will be the "starter" characters when generating a word.
+#
+# - model: The Markov chain model.
+#   The model is two-level dictionary.
+#   The first key is the two char combo that forms the current state.
+#   The second key is a possible next character.
+#   For example:
+#       model["ee"]["n"] = 5
+#   means we have encountered the letter "n" after "ee" five times in our
+#   dictionary of words.
+
 start_chars = defaultdict(int)
 model = defaultdict(lambda: defaultdict(int))
 
+# Go through each word in the dictionary to develop the model
 for word in words:
-    prev = word[0:2]
-    start_chars[prev] += 1
-    for c in word[2:]:
-        model[prev][c] += 1
-        prev = prev[1] + c
-    model[prev]["."] += 1
+    # The first two chars of the word form our starting point
+    context = word[0:2]
 
-for prev in model.keys():
-    model[prev] = dict(
-        accumulate(model[prev].items(), lambda a, b: (b[0], a[1] + b[1]))
+    # Save off the fact these two chars start a word
+    start_chars[context] += 1
+
+    # Step through the rest of the word, building the model
+    for c in word[2:]:
+        model[context][c] += 1
+        context = context[1] + c
+
+    # Save off that we've reached the end of a word
+    # using "." as the end-of-word marker.
+    model[context]["."] += 1
+
+# This step isn't completely necessary. We want to provide a probability
+# distribution for random.choices. Using cumulative weights saves work for
+# that function.
+
+for context in model.keys():
+    model[context] = dict(
+        accumulate(model[context].items(), lambda a, b: (b[0], a[1] + b[1]))
     )
 
 start_chars = dict(accumulate(start_chars.items(), lambda a, b: (b[0], a[1] + b[1])))
+
+# Save the model of gzip'd JSON
 
 with gzip.open(sys.argv[2], "wt") as f:
     json.dump(
